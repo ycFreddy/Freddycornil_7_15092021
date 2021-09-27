f05909e7df655a9f4ddff6c3329c2026b506e637
ycFreddy <fredc@netc.fr>
2021-09-27 10:24:23 +0200

    MAJ Moteur de Recherche

--- base.js
+++ base.js
@@ -1,22 +1,74 @@
-
 class urlParams {
   static returnUrlRequete () {
-    return new URL(document.location.href).searchParams.get('requete')
+    const req = new URL(document.location.href).searchParams.get('requete')
+    const reqIngredients = new URL(document.location.href).searchParams.get('ingredients')
+    const reqAppareils = new URL(document.location.href).searchParams.get('appareils')
+    const reqUstensiles = new URL(document.location.href).searchParams.get('ustensiles')
+    if (reqIngredients) { return reqIngredients }
+    if (req) { return req }
+    if (reqAppareils) { return reqAppareils }
+    if (reqUstensiles) { return reqUstensiles }
   }
 }
 
 const rechercher = (value) => {
   const resultIngredient = recipes.filter(({ ingredients }) => ingredients.find(({ ingredient }) => ingredient === value))
   const resultUstensile = recipes.filter(({ ustensils }) => ustensils.includes(value))
-  const resultNomRecette = recipes.filter(({ name }) => name === value)
+  let resultNomRecette = recipes.filter(({ name }) => name.toUpperCase().search(value.toUpperCase()) !== -1)
   const resultAppareil = recipes.filter(({ appliance }) => appliance === value)
   if (resultIngredient.length > 0) return resultIngredient
   if (resultUstensile.length > 0) return resultUstensile
-  if (resultNomRecette.length > 0) return resultNomRecette
   if (resultAppareil.length > 0) return resultAppareil
+  if (resultNomRecette.length > 0) { return resultNomRecette } else { resultNomRecette = []; return resultNomRecette }
 }
 
-//console.log(rechercher(urlParams.returnUrlRequete()))
+let ligne = 0
+let l
+const parent = document.getElementById('resultats')
+const afficheCarte = (el, parent) => {
+  const carte = document.createElement('div')
+  carte.className = 'card'
+  parent.appendChild(carte)
+  const carteImg = document.createElement('img')
+  carteImg.className = 'card-img-top'
+  carteImg.src = 'public/images/img.jpg'
+  carte.appendChild(carteImg)
+  const carteBody = document.createElement('div')
+  carteBody.className = 'card-body'
+  carte.appendChild(carteBody)
+  const carteTitre = document.createElement('h5')
+  carteTitre.className = 'card-title'
+  carteTitre.innerHTML = '<div class="d-flex justify-content-between m-0 p-0"><p>' + el.name + '</p><p class="text-nowrap font-weight-bold"><i class="far fa-clock pr-2"></i>' + el.time + 'min</p></div>'
+  carteBody.appendChild(carteTitre)
+  const carteTexte = document.createElement('div')
+  carteTexte.className = 'card-text d-flex justify-content-between carte'
+  carteBody.appendChild(carteTexte)
+  const listeIngredients = document.createElement('div')
+  carteTexte.appendChild(listeIngredients)
+  listeIngredients.className = 'carte'
+  el.ingredients.forEach(val => {
+    if (val.quantity === undefined) val.quantity = ''
+    if (val.unit === undefined) val.unit = ''
+    listeIngredients.innerHTML += '<p><strong>' + val.ingredient + '</strong>: ' + val.quantity + ' ' + val.unit + '</p>'
+  })
+  carteTexte.innerHTML += '<div class="col-6 m-0 p-0 pl-2 description">' + el.description + '</div>'
+}
+
+if (urlParams.returnUrlRequete() !== undefined) {
+  rechercher(urlParams.returnUrlRequete()).forEach(el => {
+    if ((ligne % 3) === 0) {
+      l = document.createElement('div')
+      l.name = el.id + 'row'
+      l.className = 'row'
+      parent.appendChild(l)
+    }
+    ligne++
+    const r = document.createElement('div')
+    r.className = 'col-4 p-2'
+    l.appendChild(r)
+    afficheCarte(el, r)
+  })
+}
 
 const listIngredients = []
 const listUstensiles = []
@@ -89,19 +141,20 @@ function autocomplete (inp, arr, type, color) {
   })
   const addCol = (d, i, val, type) => {
     const b = document.createElement('div')
-    b.className = 'col-4 ' + type + 'col'
+    b.className = 'col w-400 ' + type + 'col colonne'
     b.innerHTML = '<strong>' + i.substr(0, val.length) + '</strong>'
     b.innerHTML += i.substr(val.length)
     b.innerHTML += '<input type="hidden" value="' + i + '">'
     b.addEventListener('click', (e) => {
       inp.value = b.getElementsByTagName('input')[0].value
+      inp.form.submit()
       closeAllLists()
     })
     d.appendChild(b)
   }
   inp.addEventListener('keydown', (e) => {
     let x = document.getElementById(inp.id + 'autocomplete-list')
-    if (x) x = x.getElementsByClassName('col-4')
+    if (x) x = x.getElementsByClassName('colonne')
     if ((e.keyCode === 39)) {
       currentFocus++
       addActive(x)
