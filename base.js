85e7a723af076d8c2dfd078279b7b30feb0dd751
ycFreddy <fredc@netc.fr>
2021-09-29 20:13:41 +0200

    MAJ Moteur de Recherche

--- base.js
+++ base.js
@@ -1,50 +1,14 @@
-/* eslint-disable no-trailing-spaces */
-class urlParams {
-  static returnTabRequete () {
-    const req = new URL(document.location.href).searchParams.get('requete')
-    const reqIngredients = new URL(document.location.href).searchParams.get('ingredients')
-    const reqAppareils = new URL(document.location.href).searchParams.get('appareils')
-    const reqUstensiles = new URL(document.location.href).searchParams.get('ustensiles')
-    if (reqIngredients) { return reqIngredients }
-    if (req) { return req }
-    if (reqAppareils) { return reqAppareils }
-    if (reqUstensiles) { return reqUstensiles }
-  }
-  
-  static returnUrlRequete () {
-    const type = []
-    const urlIngredients = new URL(document.location.href).searchParams.get('ingredients')
-    const urlAppareils = new URL(document.location.href).searchParams.get('appareils')
-    const urlUstensiles = new URL(document.location.href).searchParams.get('ustensiles')
-    if (urlIngredients) {
-      type.push({ type: 'ingredient', valeur: urlIngredients })
-      return type
-    }
-    if (urlAppareils) {
-      type.push({ type: 'appareil', valeur: urlAppareils })
-      return type
-    }
-    if (urlUstensiles) {
-      type.push({ type: 'ustensile', valeur: urlUstensiles })
-      return type
-    }
-  }
-}
-
 const rechercher = (value) => {
-  const resultIngredient = recipes.filter(({ ingredients }) => ingredients.find(({ ingredient }) => ingredient === value))
-  const resultUstensile = recipes.filter(({ ustensils }) => ustensils.includes(value))
+  const resultIngredient = recipes.filter(({ ingredients }) => ingredients.find(({ ingredient }) => ingredient.toUpperCase() === value.toUpperCase()))
+  const resultUstensile = recipes.filter(({ ustensils }) => ustensils.find(el => el.toUpperCase() === value.toUpperCase()))
   let resultNomRecette = recipes.filter(({ name }) => name.toUpperCase().search(value.toUpperCase()) !== -1)
-  const resultAppareil = recipes.filter(({ appliance }) => appliance === value)
+  const resultAppareil = recipes.filter(({ appliance }) => appliance.toUpperCase() === value.toUpperCase())
   if (resultIngredient.length > 0) return resultIngredient
   if (resultUstensile.length > 0) return resultUstensile
   if (resultAppareil.length > 0) return resultAppareil
   if (resultNomRecette.length > 0) { return resultNomRecette } else { resultNomRecette = []; return resultNomRecette }
 }
 
-let ligne = 0
-let l
-const parent = document.getElementById('resultats')
 const afficheCarte = (el, parent) => {
   const carte = document.createElement('div')
   carte.className = 'card'
@@ -76,38 +40,103 @@ const afficheCarte = (el, parent) => {
 
 // Affiche de le menu de tags uniques
 const ProcessMenuTags = (data) => {
-  const parent = document.getElementById('rechercher')
-  const tag = document.createElement('div')
-  tag.className = 'alert d-flex align-items-center justify-content-between m-2 p-02'
-  if (data[0].type === 'ingredient') { tag.className += ' bg-primary text-white' }
-  if (data[0].type === 'appareil') { tag.className += ' bg-success text-white' }
-  if (data[0].type === 'ustensile') { tag.className += ' bg-danger text-white' }
-  tag.innerHTML = data[0].valeur
-  tag.innerHTML += '<button type="button" class="close" data-dismiss="alert"><span><i class="text-white far fa-times-circle pl-2"></i></span></button>'
-  parent.appendChild(tag)
-  tag.addEventListener('click', () => {
-    document.location.href = 'index.html'
-  })
+  const parent = document.getElementById('tags')
+  parent.innerHTML = ''
+  for (const i of data.ingredients) {
+    const tag = document.createElement('div')
+    tag.className = 'alert d-flex align-items-center justify-content-between m-2 p-02'
+    tag.className += ' bg-primary text-white'
+    tag.innerHTML = i
+    tag.innerHTML += '<button type="button" class="close" data-dismiss="alert"><span><i class="text-white far fa-times-circle pl-2"></i></span></button>'
+    parent.appendChild(tag)
+    tag.addEventListener('click', () => {
+      tabTags(i, 'ingredients', 'del')
+    })
+  }
+  for (const a of data.appareils) {
+    const tag = document.createElement('div')
+    tag.className = 'alert d-flex align-items-center justify-content-between m-2 p-02'
+    tag.className += ' bg-success text-white'
+    tag.innerHTML = a
+    tag.innerHTML += '<button type="button" class="close" data-dismiss="alert"><span><i class="text-white far fa-times-circle pl-2"></i></span></button>'
+    parent.appendChild(tag)
+    tag.addEventListener('click', () => {
+      tabTags(a, 'appareils', 'del')
+    })
+  }
+  for (const u of data.ustensiles) {
+    const tag = document.createElement('div')
+    tag.className = 'alert d-flex align-items-center justify-content-between m-2 p-02'
+    tag.className += ' bg-danger text-white'
+    tag.innerHTML = u
+    tag.innerHTML += '<button type="button" class="close" data-dismiss="alert"><span><i class="text-white far fa-times-circle pl-2"></i></span></button>'
+    parent.appendChild(tag)
+    tag.addEventListener('click', () => {
+      tabTags(u, 'ustensiles', 'del')
+    })
+  }
+  let l
+  let ligne = 0
+  const res = document.getElementById('resultats')
+  res.innerHTML = ''
+  for (let z in data) {
+    data[z].forEach(e => {
+      rechercher(e).forEach(el => {
+        if ((ligne % 3) === 0) {
+          l = document.createElement('div')
+          l.name = el.id + 'row'
+          l.className = 'row'
+          res.appendChild(l)
+        }
+        ligne++
+        const r = document.createElement('div')
+        r.className = 'col-4 p-2'
+        l.appendChild(r)
+        afficheCarte(el, r)
+      })
+    })
+  }
 }
 
-if (urlParams.returnTabRequete()) {
-  if (urlParams.returnUrlRequete()) {
-    ProcessMenuTags(urlParams.returnUrlRequete())
+const tabTag = []
+tabTag['ingredients'] = []
+tabTag['appareils'] = []
+tabTag['ustensiles'] = []
+const tabTags = (value, type, op) => {
+  if (op === 'add') {
+    tabTag[type].push(value)
+  } else {
+    console.log(tabTag[type].findIndex(e => e === value))
+    tabTag[type].splice(tabTag[type].findIndex(e => e === value), 1)
   }
-  rechercher(urlParams.returnTabRequete()).forEach(el => {
-    if ((ligne % 3) === 0) {
-      l = document.createElement('div')
-      l.name = el.id + 'row'
-      l.className = 'row'
-      parent.appendChild(l)
-    }
-    ligne++
-    const r = document.createElement('div')
-    r.className = 'col-4 p-2'
-    l.appendChild(r)
-    afficheCarte(el, r)
-  })
+  ProcessMenuTags(tabTag)
 }
+
+const requete = document.getElementById('requete')
+requete.addEventListener('input', () => {
+  let l
+  let ligne = 0
+  const parent = document.getElementById('resultats')
+  parent.innerHTML = ''
+  if (requete.value.length > 2) {
+    rechercher(requete.value).forEach(el => {
+      if ((ligne % 3) === 0) {
+        l = document.createElement('div')
+        l.name = el.id + 'row'
+        l.className = 'row'
+        parent.appendChild(l)
+      }
+      ligne++
+      const r = document.createElement('div')
+      r.className = 'col-4 p-2'
+      l.appendChild(r)
+      afficheCarte(el, r)
+    })
+  }
+})
+requete.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') { e.preventDefault() }
+})
 
 const listIngredients = []
 const listUstensiles = []
@@ -126,15 +155,15 @@ for (const i of recipes) {
 }
 
 const autocomplete = (inp, arr, type, color) => {
-  let currentFocus  
-  inp.addEventListener('click', () => {    
+  let currentFocus
+  inp.addEventListener('click', () => {
     closeAllLists()
     document.getElementById(inp.id + 'chevron').classList.add('up-chevron')
     document.getElementById(inp.id + 'chevron').classList.remove('down-chevron')
     const a = document.createElement('div')
     a.id = inp.id + 'autocomplete-list'
     a.className = 'autocomplete-items p-2 ' + color + ' text-light down-items-list'
-    inp.parentNode.appendChild(a) 
+    inp.parentNode.appendChild(a)
     currentFocus = -1
     let row = 0
     let d
@@ -153,11 +182,11 @@ const autocomplete = (inp, arr, type, color) => {
           a.appendChild(d)
         }
         row++
-        addCol(d, i, '')
+        addCol(d, i, '', type)
       }
     }
   })
-  inp.addEventListener('input', () => {    
+  inp.addEventListener('input', () => {
     closeAllLists()
     document.getElementById(inp.id + 'chevron').classList.add('up-chevron')
     document.getElementById(inp.id + 'chevron').classList.remove('down-chevron')
@@ -178,11 +207,11 @@ const autocomplete = (inp, arr, type, color) => {
           a.appendChild(d)
         }
         row++
-        addCol(d, i, val)
+        addCol(d, i, val, type)
       }
     }
   })
-  const addCol = (d, i, val) => {
+  const addCol = (d, i, val, type) => {
     const b = document.createElement('div')
     b.className = 'col w-400 colonne'
     b.innerHTML = '<strong>' + i.substr(0, val.length) + '</strong>'
@@ -190,7 +219,7 @@ const autocomplete = (inp, arr, type, color) => {
     b.innerHTML += '<input type="hidden" value="' + i + '">'
     b.addEventListener('click', (e) => {
       inp.value = b.getElementsByTagName('input')[0].value
-      inp.form.submit()
+      tabTags(inp.value, type, 'add')
       closeAllLists()
     })
     d.appendChild(b)
@@ -198,21 +227,22 @@ const autocomplete = (inp, arr, type, color) => {
   inp.addEventListener('keydown', (e) => {
     let x = document.getElementById(inp.id + 'autocomplete-list')
     if (x) x = x.getElementsByClassName('colonne')
-    if ((e.keyCode === 39)) {
+    if ((e.key === 'ArrowRight')) {
       currentFocus++
       addActive(x)
-    } else if (e.keyCode === 40) {
+    } else if (e.key === 'ArrowDown') {
       currentFocus = currentFocus + 3
       addActive(x)
-    } else if (e.keyCode === 37) {
+    } else if (e.key === 'ArrowLeft') {
       currentFocus--
       addActive(x)
-    } else if (e.keyCode === 38) {
+    } else if (e.key === 'ArrowUp') {
       currentFocus = currentFocus - 3
       addActive(x)
-    } else if (e.keyCode === 13) {
+    } else if (e.key === 'Enter') {
       if (currentFocus > -1) {
         if (x) x[currentFocus].click()
+        e.preventDefault()
       }
     }
   })
@@ -229,11 +259,11 @@ const autocomplete = (inp, arr, type, color) => {
       i.classList.remove('autocomplete-active')
     }
   }
-  const closeAllLists = (el) => {    
+  const closeAllLists = (el) => {
     const x = document.getElementsByClassName('autocomplete-items')
     for (const i of x) {
-      document.getElementById(i.id.replace('autocomplete-list', '') + 'chevron').classList.remove('up-chevron') 
-      document.getElementById(i.id.replace('autocomplete-list', '') + 'chevron').classList.add('down-chevron')      
+      document.getElementById(i.id.replace('autocomplete-list', '') + 'chevron').classList.remove('up-chevron')
+      document.getElementById(i.id.replace('autocomplete-list', '') + 'chevron').classList.add('down-chevron')
       if (el !== i && el !== inp) {
         i.parentNode.removeChild(i)
       }
@@ -247,4 +277,3 @@ const autocomplete = (inp, arr, type, color) => {
 autocomplete(document.getElementById('ingredients'), listIngredients, 'ingredients', 'bg-primary')
 autocomplete(document.getElementById('appareils'), listAppareils, 'appareils', 'bg-success')
 autocomplete(document.getElementById('ustensiles'), listUstensiles, 'ustensiles', 'bg-danger')
-
